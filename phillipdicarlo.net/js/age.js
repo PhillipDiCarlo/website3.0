c38b1dc43a182a34b5073da69459df2c7acffb0e
PhillipDiCarlo <34153910+PhillipDiCarlo@users.noreply.github.com>
2022-01-26 07:04:45 -0500

    Changed birthday to be in a better format

--- phillipdicarlo.net/js/age.js
+++ phillipdicarlo.net/js/age.js
@@ -1,6 +1,7 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = dayjs().diff(dayjs(0852001960000), 'year', true); //This is set to my birthday
+	var d = new Date(1996, 8, 26); //This is set to my birthday
+	let time = dayjs().diff(dayjs(d), 'year', true); 
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
