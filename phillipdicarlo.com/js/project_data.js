bcbcb7a9488a13ee2e25c311841366c01c90b2cb
Phillip DiCarlo <34153910+PhillipDiCarlo@users.noreply.github.com>
2022-08-18 20:58:57 -0400

    Added new project to tab and currently working on section

--- phillipdicarlo.com/js/project_data.js
+++ phillipdicarlo.com/js/project_data.js
@@ -1,42 +1,55 @@
 const ProjectData = {
-	// "<VAR_NAME>": {
-	// 	title: "",
-	// 	github: "",
-	// 	description: ""
-	// },
-	"tempo_timeKeep": {
-		title: "TEMPO TimeKeeper",
-		github: "https://github.com/PhillipDiCarlo/Tempo-Time-Keeper",
-		description: "This was an application I created during my first internship to solve a timekeeping issue. Working for a government contractor, we needed to keep track of time spent on each contract number. Traditionally you needed to manually remember start and finish times for each contract ID. This program consolidates all contracts worked on each day and displays an easy to read output."
-	},
-	"theCorruptor": {
-		title: "The Corruptor",
-		github: "https://github.com/PhillipDiCarlo/The-Corruptor",
-		description: "In conjunction with ASRC Federal, this project was an in-house product that took incoming message data and modifies it to generate hard to create scenarios, reducing the time to create custom tests."
-	},
-	"portfolio": {
-		title: "Portfolio",
-		github: "https://github.com/PhillipDiCarlo/website3.0",
-		description: "You're looking at it! This website was a lot of work and I'm really proud of some of the results I got."
-	},
-	"webTester": {
-		title: "Web Tester",
-		github: "https://github.com/PhillipDiCarlo/web-programming-final",
-		description: "This was a project where I created a website that enabled multiple visitors to chat with one another in a lobby."
-	},
-	"facialRecog": {
-		title: "Facial Recognition",
-		github: "",
-		description: "Setup a home security system and wanted to take it a step further and detect when unknown faces approached my house and catalogued them by similarity. The system I decided to go with was a mix of OpenAI and DeepStackAI."
-	},
-	"achievementHunter": {
-		title: "Achievement Hunter",
-		github: "",
-		description: "Written as an effort to solve the issue with cross platform achievement syncing. Hosted on AWS, and using OAuth as the main login system I utlized available API's with Steam, Origin, Xbox, and Playstation to view client data such as games played and achievements and created a single database where users could view all their achievements from every platform and compare them with friends and global users."
-	},
-	"redditScraper": {
-		title: "Reddit Scraper Bot",
-		github: "",
-		description: "Famous Twitch streamer was becoming irate of a Reddit community's subreddit they frequently visited and showed on stream. I created a new subreddit for them, with identical posts without the user's ability to make comments. <br /> In order to make the posts identical, I automated a bot that scanned for any new posts with a positive upvote and post the identitcal content over an interval to the new subreddit. <br /> Programmed mostly in Javascript."
-	},
+  // "<VAR_NAME>": {
+  // 	title: "",
+  // 	github: "",
+  // 	description: ""
+  // },
+  tempo_timeKeep: {
+    title: "TEMPO TimeKeeper",
+    github: "https://github.com/PhillipDiCarlo/Tempo-Time-Keeper",
+    description:
+      "This was an application I created during my first internship to solve a timekeeping issue. Working for a government contractor, we needed to keep track of time spent on each contract number. Traditionally you needed to manually remember start and finish times for each contract ID. This program consolidates all contracts worked on each day and displays an easy to read output.",
+  },
+  theCorruptor: {
+    title: "The Corruptor",
+    github: "https://github.com/PhillipDiCarlo/The-Corruptor",
+    description:
+      "In conjunction with ASRC Federal, this project was an in-house product that took incoming message data and modifies it to generate hard to create scenarios, reducing the time to create custom tests.",
+  },
+  portfolio: {
+    title: "Portfolio",
+    github: "https://github.com/PhillipDiCarlo/website3.0",
+    description:
+      "You're looking at it! This website was a lot of work and I'm really proud of some of the results I got.",
+  },
+  webTester: {
+    title: "P2P Website Chat",
+    github: "https://github.com/PhillipDiCarlo/web-programming-final",
+    description:
+      "This was a project where I created a website that enabled peer-to-peer chatting within different lobbies. This was written as a programming final during university.",
+  },
+  facialRecog: {
+    title: "Facial Recognition",
+    github: "",
+    description:
+      "Setup a home security system and wanted to take it a step further and detect when unknown faces approached my house and catalogued them by similarity. The system I decided to go with was a mix of OpenAI and DeepStackAI.",
+  },
+  achievementHunter: {
+    title: "Achievement Hunter",
+    github: "",
+    description:
+      "Written as an effort to solve the issue with cross platform achievement syncing. Hosted on AWS, and using OAuth as the main login system I utlized available API's with Steam, Origin, Xbox, and Playstation to view client data such as games played and achievements and created a single database where users could view all their achievements from every platform and compare them with friends and global users.",
+  },
+  redditScraper: {
+    title: "Reddit Scraper Bot",
+    github: "",
+    description:
+      "Famous Twitch streamer was becoming irate of a Reddit community's subreddit they frequently visited and showed on stream. I created a new subreddit for them, with identical posts without the user's ability to make comments. <br /> In order to make the posts identical, I automated a bot that scanned for any new posts with a positive upvote and post the identitcal content over an interval to the new subreddit. <br /> Programmed mostly in Javascript.",
+  },
+  corpaStocks: {
+    title: "Twitch Streamer Stock Market",
+    github: "https://github.com/PhillipDiCarlo/corpaStocks.italiandogs",
+    description:
+      "Created a program to keep track of a streamers value determined by their viewers. Porgram lurks in a streamer's chat and listens for specific phrases. Program is written for easy scaling with new streamers easily able to be added without modifying the code in any way. Website displays a selection of tracked streamers and a graph of their current and past values. <br /> Written in Node.JS for backend, HTML and CSS for frontend, and utilizes MongoDB for the database.",
+  },
 };
